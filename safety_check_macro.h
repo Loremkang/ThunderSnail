4028b82bf012d4f9c2dcdd29df6c76775c0c0a9e
Hongbo Kang <hongbokang@icloud.com>
2023-11-14 20:45:44 +0800

    Add util functions for remoteptr. Re-define TupleIdOrMaxLinkAddrT to make it self-contained. Add more safety-check macro.

--- safety_check_macro.h
+++ safety_check_macro.h
@@ -1,11 +1,15 @@
 #pragma once
+
 #ifndef SAFETY_CHECK_MACRO_H
 #define SAFETY_CHECK_MACRO_H
 
 #include "assert.h"
 #include "stdio.h"
 
-#define overflowcheck(x) assert(x)
-#define unimplemented(x) {printf(x);assert(0);}
+#define ArrayOverflowCheck(x) assert((x))
+#define ValidValueCheck(x) assert((x))
+#define ValueOverflowCheck(x) assert((x))
+#define Unimplemented(x) {printf(x);assert(0);}
+#define NullPointerCheck(x) assert((x))
 
 #endif
