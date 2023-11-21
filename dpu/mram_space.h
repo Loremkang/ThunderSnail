008817e32dd8401e7dfd9f8b22be135029fb8c18
mryvae <1312348150@qq.com>
2023-11-21 09:27:42 +0100

    add mram space

---
+++ dpu/mram_space.h
@@ -0,0 +1,10 @@
+#pragma once
+#ifndef MRAM_SPACE_H
+#define MRAM_SPACE_H
+
+#include <mram.h>
+
+#define MAX_LINK_SPACE_ADDR ((__mram_ptr void *)(0x1000000))
+#define MAX_LINK_SPACE_SIZE (0x1000000)
+
+#endif
