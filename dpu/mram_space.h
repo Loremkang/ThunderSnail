88a7b22ac7819bf3117f3b502d9d4e6b27404a9c
mryvae <1312348150@qq.com>
2023-11-21 10:14:43 +0100

    update mram space

--- dpu/mram_space.h
+++ dpu/mram_space.h
@@ -4,7 +4,16 @@
 
 #include <mram.h>
 
+#define CPU_DPU_COMMUNICATION_SPACE_ADDR ((__mram_ptr void *)DPU_MRAM_HEAP_POINTER)
+#define CPU_DPU_COMMUNICATION_SPACE_SIZE (0x1000000 - DPU_MRAM_HEAP_POINTER)
+
 #define MAX_LINK_SPACE_ADDR ((__mram_ptr void *)(0x1000000))
 #define MAX_LINK_SPACE_SIZE (0x1000000)
+
+#define INDEX_SPACE_ADDR ((__mram_ptr void *)(0x2000000))
+#define INDEX_SPACE_SIZE (0x1000000)
+
+#define HEAP_SPACE_ADDR ((__mram_ptr void *)(0x3000000))
+#define HEAP_SPACE_SIZE (0x1000000)
 
 #endif
