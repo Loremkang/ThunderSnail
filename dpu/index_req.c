30291339524fd1f942ade89f321287b2c19e0dc9
mryvae <83715643+mryvae@users.noreply.github.com>
2023-11-24 15:20:15 +0800

    add index_req glue (#16)
    * add index_req glue
    
    * add submodule

---
+++ dpu/index_req.c
@@ -0,0 +1,43 @@
+#include "index_req.h"
+#include "shared_wram.h"
+
+primary_index_dpu *IndexCheck(uint32_t indexId){
+    return primary_index_dpu_check(indexId);
+}
+
+/*
+    usage:
+    __dma_aligned HashTableQueryReplyT reply_buffer;
+    IndexGetOrInsertReq(pid, key, keyLen, tupleID, &reply_buffer);
+*/
+void IndexGetOrInsertReq(primary_index_dpu *pid, char *key, uint32_t keyLen, TupleIdT tupleId, HashTableQueryReplyT *reply) {
+    __mram_ptr primary_index_entry *entry;
+    metadata valMeta = {.type = TupleId, .id = tupleId.tableId};
+    uint64_t val = tupleId.tupleAddr;
+
+    entry = primary_index_dpu_get_or_insert(pid, key, keyLen, valMeta, val);
+    if (entry) {
+        __dma_aligned primary_index_entry entry_buffer;
+        mram_read((__mram_ptr void *)entry, (void *)&entry_buffer, sizeof(primary_index_entry));
+        if (entry_buffer.val_meta.type == TupleId) {
+            reply->type = TupleId;
+            reply->value.tupleId.tableId = entry_buffer.val_meta.id;
+            reply->value.tupleId.tupleAddr = entry_buffer.val;
+        } else if (entry_buffer.val_meta.type == MaxLinkAddr) {
+            reply->type = MaxLinkAddr;
+            reply->value.maxLinkAddr.rPtr = RemotePtrFromI64(entry_buffer.val);
+        }
+    } else {
+        entry = primary_index_dpu_lookup(pid, key, keyLen);
+        reply->type = HashAddr;
+        reply->value.hashAddr.rPtr.dpuId = g_dpuId;
+        reply->value.hashAddr.rPtr.dpuAddr = (uint32_t)entry;
+    }
+}
+
+void IndexUpdateReq(primary_index_dpu *pid, char *key, HashAddrT hashAddr, MaxLinkAddrT maxLinkAddr) {
+    __mram_ptr primary_index_entry *entry = (__mram_ptr primary_index_entry *)hashAddr.rPtr.dpuAddr;
+    metadata newValMeta = {.type = MaxLinkAddr};
+    uint64_t newVal = RemotePtrToI64(maxLinkAddr.rPtr);
+    primary_index_dpu_update_with_entry_addr(pid, entry, newValMeta, newVal);
+}
---
+++ dpu/shared_wram.h
@@ -0,0 +1,9 @@
+#pragma once
+#ifndef SHARED_WRAM_H
+#define SHARED_WRAM_H
+
+#include <stdint.h>
+
+extern uint32_t g_dpuId;
+
+#endif
