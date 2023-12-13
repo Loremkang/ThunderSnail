c66bf974f077c4462d950444d6c84dc6adef4343
Hongbo Kang <hongbokang@icloud.com>
2023-12-13 16:19:41 +0800

    driver draft.

--- driver/driver.c
+++ driver/driver.c
@@ -1,9 +1,10 @@
 #include "driver.h"
 #include "fake_interface.h"
 #include "safety_check_macro.h"
-#include "dpu_buffer_builder.h"
+#include "cpu_buffer_builder.h"
 #include "protocol.h"
 #include "shared_constants.h"
+#include "io_manager.h"
 
 #define PRIMARY_INDEX_MAX_NUM 16
 
@@ -16,15 +17,13 @@ static inline uint32_t _hash_function(uint8_t *buf, uint32_t len)
     return hash;
 }
 
-bool hashTableInitialized[PRIMARY_INDEX_MAX_NUM] = {false};
-
-void BuildGetOrInsertReqOnHashTableIndex(int batchSize, int TableId,
-                                         TupleIdT *tupleIds,
-                                         TupleIdT *resultTupleIds,
-                                         HashTableQueryReplyT *resultCounterpart) {
+void RunGetOrInsert(IOManagerT ioManager, struct dpu_set_t* set, int batchSize, int TableId, TupleIdT *tupleIds,
+                         TupleIdT *resultTupleIds,
+                         HashTableQueryReplyT *resultCounterpart) {
+    static bool hashTableInitialized[PRIMARY_INDEX_MAX_NUM] = {false};
     int hashTableCount = CatalogHashTableCountGet(TableId);
     for (int hashTableIdx = 0; hashTableIdx < hashTableCount; hashTableIdx++) {
-        int hashTableId = CatalogHashTableIdGet(TableId, hashTableIdx);
+        int hashTableId = CatalogEdgeIdGet(TableId, hashTableIdx);
         ArrayOverflowCheck(hashTableId < PRIMARY_INDEX_MAX_NUM);
         if (!hashTableInitialized[hashTableId]) {
             hashTableInitialized[hashTableId] = true;
@@ -32,49 +31,40 @@ void BuildGetOrInsertReqOnHashTableIndex(int batchSize, int TableId,
         }
     }
 
-    uint8_t *buffers[NUM_DPU];
-    size_t sizes[NUM_DPU];
-    BufferBuilder builders[NUM_DPU];
-    CpuToDpuBufferDescriptor bufferDescs[NUM_DPU];
-    for (int i = 0; i < NUM_DPU; i++) {
-        memset(&bufferDescs[i], 0, sizeof(CpuToDpuBufferDescriptor));
-        bufferDescs[i] =
-            (CpuToDpuBufferDescriptor){.header = {
-                                           .epochNumber = GetEpochNumber(),
-                                       }};
-    }
-
-    for (int i = 0; i < NUM_DPU; i++) {
-        BufferBuilderInit(&builders[i], &bufferDescs[i]);
-        BufferBuilderBeginBlock(&builders[i], GET_OR_INSERT_REQ);
-    }
+    IOManagerInit(&ioManager, &set, GlobalIOBuffers, GlobalOffsetsBuffer,
+                  GlobalVarlenBlockOffsetBuffer, GlobalIOBuffers);
+    IOManagerBeginBlock(&ioManager, GET_OR_INSERT_REQ);
 
     for (int hashTableIdx = 0; hashTableIdx < hashTableCount; hashTableIdx++) {
         ValidValueCheck(batchSize > 0);
-        int hashTableId = CatalogHashTableIdGet(TableId, hashTableIdx);
-        int keyLength = CatalogHashTableKeyLengthGet(hashTableId)
+        int hashTableId = CatalogEdgeIdGet(TableId, hashTableIdx);
+        int keyLength = CatalogHashTableKeyLengthGet(hashTableId);
         uint8_t *key = malloc(keyLength);
 
-        size_t taskSize =
-                        ROUND_UP_TO_8(keyLength + sizeof(GetOrInsertReq));
+        size_t taskSize = ROUND_UP_TO_8(keyLength + sizeof(GetOrInsertReq));
         GetOrInsertReq *req = malloc(taskSize);
         memset(req, 0, taskSize);
         req->base = (Task){.taskType = GET_OR_INSERT_REQ};
         req->len = keyLength;
         req->hashTableId = hashTableId;
-        
+
         for (int i = 0; i < batchSize; i++) {
             CatalogHashTableKeyGet(tupleIds[i], hashTableIdx, key);
             int dpuIdx = _hash_function(key, keyLength) % NUM_DPU;
-            
             req->tid = tupleIds[i];
-            
             memcpy(req->ptr, keys[i].data, keys[i].len);
             // append one task for each dpu
-            BufferBuilderAppendTask(&builders[dpuIdx], (Task *)req);
+            IOManagerAppendTask(&ioManager, dpuIdx, (Task *)req);
         }
         free(key);
+        free(req);
     }
+
+    IOManagerEndBlock(&ioManager);
+    IOManagerFinish(&ioManager);
+
+    IOManagerSendExecReceive(&ioManager);
+
 }
 
 void BatchInsertValidCheck(int batchSize, TupleIdT *tupleIds) {
@@ -88,7 +78,7 @@ void BatchInsertValidCheck(int batchSize, TupleIdT *tupleIds) {
     }
 }
 
-void BatchInsertTuple(int batchSize, TupleIdT *tupleIds) {
+void BatchInsertTuple(int batchSize, TupleIdT *tupleIds, struct dpu_set_t* set, IOManagerT *ioManager) {
     BatchInsertValidCheck(batchSize, tupleIds);
 
     TupleIdT resultTupleIds[MAXSIZE_HASH_TABLE_QUERY_BATCH];
@@ -96,46 +86,7 @@ void BatchInsertTuple(int batchSize, TupleIdT *tupleIds) {
 
     int tableId = tupleIds[0].tableId;
     int HashTableCount = CatalogHashTableCountGet(tableId);
-    for (int i = 0; i < HashTableCount; i++) {
-        BuildGetOrInsertReqOnHashTableIndex(batchSize, tableId, tupleIds,
-                                            resultTupleIds, resultCounterpart);
-    }
-}
 
-void SendGetOrInsertReq(struct dpu_set_t set, uint32_t tableId,
-                        HashTableId hashTableId, Key *keys,
-                        uint64_t *tupleAddrs, size_t batchSize,
-                        uint8_t *recvBuffers[]) {
-    // prepare dpu buffers
-
-    // end block
-    for (int i = 0; i < NUM_DPU; i++) {
-        BufferBuilderEndBlock(&builders[i]);
-        buffers[i] = BufferBuilderFinish(&builders[i], &sizes[i]);
-    }
-    // get max sizes
-    qsort(sizes, NUM_DPU, sizeof(size_t), cmpfunc);
-    // send
-    struct dpu_set_t dpu;
-
-    uint32_t idx;
-    DPU_FOREACH(set, dpu, idx) {
-        DPU_ASSERT(dpu_prepare_xfer(dpu, buffers[idx]));
-    }
-    DPU_ASSERT(dpu_push_xfer(set, DPU_XFER_TO_DPU, "receiveBuffer", 0, sizes[0],
-                             DPU_XFER_DEFAULT));
-    DPU_ASSERT(dpu_launch(set, DPU_SYNCHRONOUS));
-    // ReadDpuSetLog(set);
-    // receive
-    DPU_FOREACH(set, dpu, idx) {
-        DPU_ASSERT(dpu_prepare_xfer(dpu, recvBuffers[idx]));
-    }
-    // how to get the reply buffer size? it seems that the reply buffer size
-    // will less then send buffer size
-    DPU_ASSERT(dpu_push_xfer(set, DPU_XFER_FROM_DPU, "replyBuffer", 0, sizes[0],
-                             DPU_XFER_DEFAULT));
-    // free
-    for (int i = 0; i < NUM_DPU; i++) {
-        free(buffers[i]);
-    }
+    RunGetOrInsert(ioManager, set, batchSize, tableId, tupleIds, resultTupleIds,
+                        resultCounterpart);
 }
--- driver/driver.h
+++ driver/driver.h
@@ -5,6 +5,6 @@
 #include "common_base_struct.h"
 #include "requests.h"
 
-void BatchInsertTuple(int, TupleIdT*);
+void BatchInsertTuple(int batchSize, TupleIdT *tupleIds, struct dpu_set_t* set, IOManagerT *ioManager);
 
 #endif
